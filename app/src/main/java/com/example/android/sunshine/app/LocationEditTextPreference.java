3bd8357d54f442635e3a6ad2c889fb2ce8185cc7
Eduard Bolos <eduard@novoda.com>
2016-04-07 16:53:04 +0200

    Enforce location minimum 3 chars long

---
+++ app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
@@ -0,0 +1,80 @@
+package com.example.android.sunshine.app;
+
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class LocationEditTextPreference extends EditTextPreference {
+
+    private static final int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
+
+    private int mMinLenght = DEFAULT_MINIMUM_LOCATION_LENGTH;
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public LocationEditTextPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    public LocationEditTextPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    public LocationEditTextPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public LocationEditTextPreference(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        if (attrs != null) {
+
+            TypedArray a = context.getTheme().obtainStyledAttributes(
+                    attrs, R.styleable.LocationEditTextPreference, 0, 0);
+
+            try {
+                mMinLenght = a.getInteger(R.styleable.LocationEditTextPreference_minLenght, DEFAULT_MINIMUM_LOCATION_LENGTH);
+            } finally {
+                a.recycle();
+            }
+        }
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+
+        getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Dialog dialog = getDialog();
+                if (dialog instanceof AlertDialog) {
+                    Button positiveButton = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                    positiveButton.setEnabled(s.length() >= mMinLenght);
+                }
+            }
+        });
+    }
+}
