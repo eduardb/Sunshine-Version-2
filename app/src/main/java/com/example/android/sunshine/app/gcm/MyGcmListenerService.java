30af414b39090bf1808d3091fa83e75df2845c66
Eduard Bolos <eduard@novoda.com>
2016-05-01 16:49:39 +0200

    Handle received GCM messages

--- app/src/main/java/com/example/android/sunshine/app/gcm/MyGcmListenerService.java
+++ app/src/main/java/com/example/android/sunshine/app/gcm/MyGcmListenerService.java
@@ -1,6 +1,58 @@
 package com.example.android.sunshine.app.gcm;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.example.android.sunshine.app.MainActivity;
+import com.example.android.sunshine.app.R;
 import com.google.android.gms.gcm.GcmListenerService;
 
 public class MyGcmListenerService extends GcmListenerService {
+
+    private static final String TAG = "MyGcmListenerService";
+    private static final String EXTRA_WEATHER = "weather";
+    private static final String EXTRA_LOCATION = "location";
+    private static final int NOTIFICATION_ID = 1;
+
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        if (data.isEmpty()) {
+            return;
+        }
+        // Not a bad idea to check that the message is coming from our server
+        if (!getString(R.string.gcm_defaultSenderId).equals(from)) {
+            return;
+        }
+
+        String weather = data.getString(EXTRA_WEATHER);
+        String location = data.getString(EXTRA_LOCATION);
+        String alert = getString(R.string.gcm_weather_alert_message, weather, location);
+
+        sendNotification(alert);
+
+        Log.i(TAG, "Received: " + data.toString());
+    }
+
+    private void sendNotification(String message) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(this.getResources(), R.drawable.art_storm);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.art_clear)
+                .setLargeIcon(largeIcon)
+                .setContentTitle(getString(R.string.gcm_weather_alert_title))
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(contentIntent);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
 }
